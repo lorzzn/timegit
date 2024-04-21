66d71e254f1907520cdef3c0712ee42e750c0973
lorenzozane <255499213@qq.com>
2024-04-21 17:51:11 +0800

    update: add trpc

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,20 +1,23 @@
 import { NextAuthProvider } from "@/components/NextAuthProvider"
 import Content from "@/layout/components/Content"
 import Header from "@/layout/components/Header"
+import Provider from "@/trpc/client/provider"
 import { twclx } from "@/utils/twclx"
 import { NextUIProvider } from "@nextui-org/react"
 import { PropsWithChildren } from "react"
 
 const Layout = (props: PropsWithChildren) => {
   return (
-    <NextAuthProvider>
-      <NextUIProvider>
-        <div className={twclx(["min-h-screen flex flex-col w-full"])}>
-          <Header />
-          <Content>{props.children}</Content>
-        </div>
-      </NextUIProvider>
-    </NextAuthProvider>
+    <Provider>
+      <NextAuthProvider>
+        <NextUIProvider>
+          <div className={twclx(["min-h-screen flex flex-col w-full"])}>
+            <Header />
+            <Content>{props.children}</Content>
+          </div>
+        </NextUIProvider>
+      </NextAuthProvider>
+    </Provider>
   )
 }
 
---
+++ src/trpc/client/provider.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { getBaseUrl } from "@/utils/tools"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { httpBatchLink } from "@trpc/client"
+import { PropsWithChildren, useState } from "react"
+import { trpc } from "."
+
+const Provider = ({ children }: PropsWithChildren) => {
+  const [queryClient] = useState(() => new QueryClient())
+
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: `${getBaseUrl()}/api/trpc`,
+        }),
+      ],
+    }),
+  )
+
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </trpc.Provider>
+  )
+}
+
+export default Provider
